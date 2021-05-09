6e9bd2e9ddbf6b6c0ebf4cc93ca85bcf4922aad2
DESKTOP-939248N\apple <fivestar201212@gmail.com>
2021-05-10 08:33:29 +0900

    make init structure in demo HTML

--- src/index.js
+++ src/index.js
@@ -13,16 +13,19 @@ const CoCreateResizeObserver = {
 		this.rValue = '';
 		
 	}
+
+	// initElement: function()
 }
 
 // New ResizeObserver for observing
  CoCreateResizeObserver.prototype.resizeObserver = new ResizeObserver(entries => {
 	for (let entry of entries) {
 	  if(entry.contentBoxSize) {
-		
+		console.log(entry.contentBoxSize.inlineSize);
 		// h1Elem.style.fontSize = Math.max(1.5, contentBoxSize.inlineSize / 200) + 'rem';
 		// pElem.style.fontSize = Math.max(1, contentBoxSize.inlineSize / 600) + 'rem';
 	  } else {
+		  console.log(entry.contentRect.width);
 		// h1Elem.style.fontSize = Math.max(1.5, entry.contentRect.width / 200) + 'rem';
 		// pElem.style.fontSize = Math.max(1, entry.contentRect.width / 600) + 'rem';
 	  }
