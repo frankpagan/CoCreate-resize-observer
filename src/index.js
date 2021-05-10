dcaa095362cff4fd449b22b241ad71372c2c7d42
DESKTOP-939248N\apple <fivestar201212@gmail.com>
2021-05-10 09:12:22 +0900

    fix bugs

--- src/index.js
+++ src/index.js
@@ -1,44 +1,25 @@
-const CoCreateResizeObserver = {
-  
+function CoCreateResizeObserver(selector, target, property, value)
+{
+	this.selector = selector;
+	this.target = target;
+	this.property = property;
+	this.value = value;
+}
+
+// New ResizeObserver for observing
+ CoCreateResizeObserver.prototype = {
+	
 	init: function(container) {
 		let mainContainer = container || document;
 		if (!mainContainer.querySelectorAll) {
 			return;
 		}
 		
-		// 3 properties used in this task
-		// rElements are target
-		this.rElements = mainContainer.querySelectorAll('[data-namespace], [data-room]');
-		this.rProperty = '';
-		this.rValue = '';
-		
-	}
-
-	// initElement: function()
-}
-
-// New ResizeObserver for observing
- CoCreateResizeObserver.prototype.resizeObserver = new ResizeObserver(entries => {
-	for (let entry of entries) {
-	  if(entry.contentBoxSize) {
-		console.log(entry.contentBoxSize.inlineSize);
-		// h1Elem.style.fontSize = Math.max(1.5, contentBoxSize.inlineSize / 200) + 'rem';
-		// pElem.style.fontSize = Math.max(1, contentBoxSize.inlineSize / 600) + 'rem';
-	  } else {
-		  console.log(entry.contentRect.width);
-		// h1Elem.style.fontSize = Math.max(1.5, entry.contentRect.width / 200) + 'rem';
-		// pElem.style.fontSize = Math.max(1, entry.contentRect.width / 600) + 'rem';
-	  }
-	}
+	},
 	
-	console.log('Size changed');
-  });
+	resizeObserver: function() {
+
+	}
+}
   
-  resizeObserver.observe(divElem);
-
-//Main Execute
-CustomResizeObserver = new CoCreateResizeObserver();
-CustomResizeObserver.init();
-CustomResizeObserver.resizeObserver(CustomResizeObserver.rElements);
-
 export default CoCreateResizeObserver;
