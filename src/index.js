79b3aa4c7b9f6c3563a7c6681ab701e79b40b3c2
DESKTOP-939248N\apple <fivestar201212@gmail.com>
2021-05-11 03:14:13 +0900

    init version of resize observer

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
-function CoCreateResizeObserver(selector, target, property, value)
-{
-	this.selector = selector;
-	this.target = target;
-	this.property = property;
-	this.value = value;
-}
-
-// New ResizeObserver for observing
- CoCreateResizeObserver.prototype = {
+const CoCreateResizeObserver = {
+	selector: null, 
+	target: null, 
+	property: null, 
+	value: null, 
 	
-	init: function(container) {
-		let mainContainer = container || document;
-		if (!mainContainer.querySelectorAll) {
-			return;
-		}
-		
-	},
-	
-	resizeObserver: function() {
-
-	}
-}
+	init: function (selector, target, property, value) {
+	  this.selector = selector
+	  this.target = target
+	  this.property = property
+	  this.value = value
+	  //data-resize_element
+	  const divElem = document.querySelector(this.selector); //"#resizable"
   
-export default CoCreateResizeObserver;
+	  //data-resize_target="#id"
+	  const h1Elem = document.querySelector(this.target);
+  
+	  const resizeObserver = new ResizeObserver((entries) => {
+		for (let entry of entries) {
+		  const contentBoxSize = Array.isArray(entry.contentBoxSize)
+			? entry.contentBoxSize[0]
+			: entry.contentBoxSize;
+  
+		  h1Elem.style[this.value] =
+			Math.max(1.5, entry.contentRect[this.property] / 200) + "rem";
+		}
+	  });
+	  resizeObserver.observe(divElem);
+	},
+  };
+  
+  export default CoCreateResizeObserver;
+  
