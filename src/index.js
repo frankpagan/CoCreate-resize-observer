3cbc1e2b74b755694a553ad3f7263e3dc4bad201
DESKTOP-939248N\apple <fivestar201212@gmail.com>
2021-05-12 02:04:16 +0900

    use cocreate observer

--- src/index.js
+++ src/index.js
@@ -1,29 +1,68 @@
+// "use strict";
+import observer from "@cocreate/observer";
+
+let _this;
+
 const CoCreateResizeObserver = {
-    selector: null,
-    target: null,
-    property: null,
-    value: null,
+    observer: [], //.target1, #target2
+    observerWidgets: [],
+    targets: [],
+    targetWidgets: [],
 
-    init: function(option) {
-        this.selector = option.selector;
-        this.target = option.target;
-        this.property = option.property;
-        this.value = option.value;
-
-        if (this.selector && this.target && this.property && this.value) {
-            const selectorElem = document.querySelector(this.selector);
-            const targetElem = document.querySelector(this.target);
-            const resizeObserver = new ResizeObserver((entries) => {
-                for (let entry of entries) {
-                    targetElem.style[this.value] =
-                        Math.max(1.5, entry.contentRect[this.property] / 200) + "rem";
+    init: function(handleObj) {
+        for (var handleKey in handleObj) {
+            if (handleObj.hasOwnProperty(handleKey) && handleKey == 'selector') {
+                this.observerWidgets = document.querySelectorAll(handleObj[handleKey])
+                continue;
+            }
+            if (handleObj.hasOwnProperty(handleKey) && handleKey == 'target') {
+                this.targetWidgets = document.querySelectorAll(handleObj[handleKey]);
+                continue;
+            }
+        }
+        _this = this;
+        this.observerWidgets.forEach(function(observer, i) {
+            _this.observer = observer;
+            _this.targetWidgets.forEach(function(target, index) {
+                _this.target = target;
+                if (observer == document.querySelector(target.dataset.resize_target)) {
+                    _this.property = target.dataset.resize_property;
+                    _this.value = target.dataset.resize_value;
+                    new coCreateResizeObserver(_this);
                 }
-            });
-            resizeObserver.observe(selectorElem);
-        } else {
-            console.log('Please set attributes correctly.');
+            })
+        });
+    }
+}
+
+function coCreateResizeObserver({ observer, target, property, value }) {
+    this.observer = observer;
+    this.target = target;
+    this.property = property ? property : "margin-left";
+    this.value = value ? value : "width";
+    this.init();
+}
+
+coCreateResizeObserver.prototype = {
+    init: function() {
+        if (this.observer) {
+            let __this = this;
+            observer.init({
+                name: 'ResizeObserverInit',
+                observe: ['attributes'],
+                include: __this.observer,
+                callback: function(mutation) {
+                    let changeValue = mutation.target.style[__this.value];
+                    __this.doChange(changeValue);
+                }
+            })
         }
     },
-};
+
+    doChange: function(val) {
+        this.target.style[this.property] = val;
+    },
+
+}
 
 export default CoCreateResizeObserver;
